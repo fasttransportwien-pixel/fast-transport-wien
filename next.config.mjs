05b316df788981c7e896b3f6928dcd73d39aed79
fasttransportwien-pixel <fasttransportwien@gmail.com>
2026-05-01 12:26:51 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,4 @@
-import createNextIntlPlugin from 'next-intl/plugin';
-
-const withNextIntl = createNextIntlPlugin('./src/lib/i18n.ts');
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
-  },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '10mb',
-    },
-  },
-};
+const nextConfig = {};
 
-export default withNextIntl(nextConfig);
+export default nextConfig;
